c65ee8d72d32ec2c674445f97a73d691097e9dc6
steffkes <stefan@gelungen.es>
2024-10-09 21:16:41 +0200

    refactor to single column layout

--- app.vue
+++ app.vue
@@ -1,245 +1,231 @@
 <template>
   <section class="section">
     <div class="container is-max-desktop">
-      <div class="columns">
-        <div class="column">
-          <form @submit="handleProgress">
-            <div class="field">
-              <label class="label">Name *</label>
-              <div class="control">
-                <input
-                  type="text"
-                  v-model="name"
-                  class="input"
-                  required="true"
-                  placeholder="Ihr Name"
-                />
-              </div>
-            </div>
+      <!-- 0 -->
 
-            <div class="field">
-              <label class="label">Firma *</label>
-              <div class="control">
-                <input
-                  type="text"
-                  v-model="company"
-                  class="input"
-                  required="true"
-                  placeholder="Name der Firma"
-                />
-              </div>
-            </div>
-
-            <div class="field">
-              <label class="label">eMail *</label>
-              <div class="control">
-                <input
-                  type="email"
-                  v-model="email"
-                  class="input"
-                  required="true"
-                  placeholder="Ihre eMail-Adresse"
-                />
-              </div>
-            </div>
-
-            <div class="field">
-              <label class="label">Telefon</label>
-              <div class="control">
-                <input
-                  type="text"
-                  v-model="phone"
-                  class="input"
-                  placeholder="Ihre Telefon-Nummer"
-                />
-              </div>
-            </div>
-
-            <div class="field is-grouped is-grouped-right">
-              <div class="control">
-                <button type="submit" class="button is-primary">Weiter</button>
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="column">
-          <form :class="{ 'is-hidden': step < 1 }" @submit="handleProgress">
-            <div class="field">
-              <label class="label">Stundensatz *</label>
-              <div class="field has-addons">
-                <div class="control">
-                  <input
-                    type="number"
-                    v-model="rate"
-                    class="input"
-                    required="true"
-                    placeholder="Wieviel verrechnen Sie pro Stunde?"
-                  />
-                </div>
-                <p class="control"><span class="button is-static">€</span></p>
-              </div>
-            </div>
-
-            <div class="field">
-              <label class="label">Wochenarbeitsstunden *</label>
-              <div class="control">
-                <input
-                  type="number"
-                  v-model="hours"
-                  class="input"
-                  required="true"
-                  placeholder="Wieviele Stunden arbeiten Sie pro Woche?"
-                />
-              </div>
-            </div>
-
-            <div class="field">
-              <label class="label">Büro-Anteil *</label>
-              <div class="field has-addons">
-                <div class="control">
-                  <input
-                    type="number"
-                    v-model="share"
-                    class="input"
-                    required="true"
-                    placeholder="Wieviel davon verbringen Sie im Büro?"
-                  />
-                </div>
-                <p class="control"><span class="button is-static">%</span></p>
-              </div>
-            </div>
-
-            <div class="field is-grouped is-grouped-right">
-              <div class="control">
-                <button type="submit" class="button is-primary">Weiter</button>
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="column">
-          <div :class="{ 'is-hidden': step < 2 }" class="content">
-            <div class="mb-3">
-              <p
-                v-if="weeklySavedWorkingHours >= 6"
-                style="background-color: #f00"
-              >
-                Du kannst noch einiges an Zeit und Kosten in deinem Betrieb
-                einsparen! Buche dir jetzt einen Call mit mir und lass uns
-                gemeinsam herausfinden, wie du wöchentlich bis zu
-                {{ weeklySavedCosts }} € und
-                {{ weeklySavedWorkingHours }} Stunden einsparen kannst!
-              </p>
-
-              <p
-                v-else-if="weeklySavedWorkingHours >= 3"
-                style="background-color: #ff0"
-              >
-                Da ist noch Potenzial, das du ausschöpfen kannst! Deine
-                wöchentliche Einsparung liegt bei bis zu
-                {{ weeklySavedCosts }} € Lass uns gerne darüber sprechen, wie
-                ich dich noch weiter unterstützen kann.
-              </p>
-
-              <p v-else style="background-color: #0f0">
-                Herzlichen Glückwunsch! Du bist richtig gut aufgestellt: Deine
-                Ersparnis pro Woche liegt bei bis zu {{ weeklySavedCosts }} €
-              </p>
-
-              <p v-if="weeklySavedWorkingHours >= 3">
-                <a :href="consultingLink" class="button is-primary"
-                  >Jetzt Beratungsgespräch buchen</a
-                >
-              </p>
-            </div>
-
-            <table class="table is-fullwidth">
-              <caption>
-                Deine aktuelle Zeit- und Kostenbilanz
-              </caption>
-              <thead>
-                <tr>
-                  <th>&nbsp;</th>
-                  <th>pro Woche</th>
-                  <th>pro Monat</th>
-                  <th>pro Jahr</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th>Stunden</th>
-                  <td>{{ weeklyWorkingHours }} h</td>
-                  <td>{{ perMonth(weeklyWorkingHours) }} h</td>
-                  <td>{{ perYear(weeklyWorkingHours) }} h</td>
-                </tr>
-                <tr>
-                  <th>Kosten</th>
-                  <td>{{ weeklyCosts }} €</td>
-                  <td>{{ perMonth(weeklyCosts) }} €</td>
-                  <td>{{ perYear(weeklyCosts) }} €</td>
-                </tr>
-              </tbody>
-            </table>
-
-            <table class="table is-fullwidth">
-              <caption>
-                Deine mögliche Zukunft nach unserer Zusammenarbeit
-              </caption>
-              <thead>
-                <tr>
-                  <th>&nbsp;</th>
-                  <th>pro Woche</th>
-                  <th>pro Monat</th>
-                  <th>pro Jahr</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th>Zeitersparnis</th>
-                  <td>{{ weeklySavedWorkingHours }} h</td>
-                  <td>{{ perMonth(weeklySavedWorkingHours) }} h</td>
-                  <td>{{ perYear(weeklySavedWorkingHours) }} h</td>
-                </tr>
-                <tr>
-                  <th>Kostenersparnis</th>
-                  <td>{{ weeklySavedCosts }} €</td>
-                  <td>{{ perMonth(weeklySavedCosts) }} €</td>
-                  <td>{{ perYear(weeklySavedCosts) }} €</td>
-                </tr>
-              </tbody>
-            </table>
-
-            <p>
-              Mit meiner Methode kannst du bis zu
-              {{ perYear(weeklySavedWorkingHours) }} Stunden im Jahr mehr Zeit
-              haben! Zum Beispiel für deine Familie, Freunde, Hobbies, neue
-              Kunden oder strategische Planung, um dein Unternehmen noch
-              erfolgreicher zu machen.
-            </p>
-
-            <p>
-              Mit meiner Methode kannst du bis zu
-              {{ perYear(weeklySavedCosts) }} € im Jahr einsparen, die du zum
-              Beispiel in neue Maschinen, Software, in die Weiterbildung deiner
-              Mitarbeiter oder in deine Altersvorsorge, investieren kannst.
-            </p>
-
-            <p v-if="weeklySavedWorkingHours >= 3">
-              <a :href="consultingLink" class="button is-primary"
-                >Jetzt Beratungsgespräch buchen</a
-              >
-            </p>
+      <form :class="{ 'is-hidden': step != 0 }" @submit="handleProgress">
+        <div class="field">
+          <label class="label">Name *</label>
+          <div class="control">
+            <input
+              type="text"
+              v-model="name"
+              class="input"
+              required="true"
+              placeholder="Ihr Name"
+            />
           </div>
         </div>
-      </div>
 
-      <div class="has-text-grey-lighter" style="border: 1px solid #f0f0f0">
-        <p>Step = {{ step }}</p>
+        <div class="field">
+          <label class="label">Firma *</label>
+          <div class="control">
+            <input
+              type="text"
+              v-model="company"
+              class="input"
+              required="true"
+              placeholder="Name der Firma"
+            />
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">eMail *</label>
+          <div class="control">
+            <input
+              type="email"
+              v-model="email"
+              class="input"
+              required="true"
+              placeholder="Ihre eMail-Adresse"
+            />
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Telefon</label>
+          <div class="control">
+            <input
+              type="text"
+              v-model="phone"
+              class="input"
+              placeholder="Ihre Telefon-Nummer"
+            />
+          </div>
+        </div>
+
+        <div class="field is-grouped is-grouped-right">
+          <div class="control">
+            <button type="submit" class="button is-primary">Weiter</button>
+          </div>
+        </div>
+      </form>
+
+      <!-- 1 -->
+
+      <form :class="{ 'is-hidden': step != 1 }" @submit="handleProgress">
+        <div class="field">
+          <label class="label">Stundensatz *</label>
+          <div class="field has-addons">
+            <div class="control">
+              <input
+                type="number"
+                v-model="rate"
+                class="input"
+                required="true"
+                placeholder="Wieviel verrechnen Sie pro Stunde?"
+              />
+            </div>
+            <p class="control"><span class="button is-static">€</span></p>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Wochenarbeitsstunden *</label>
+          <div class="control">
+            <input
+              type="number"
+              v-model="hours"
+              class="input"
+              required="true"
+              placeholder="Wieviele Stunden arbeiten Sie pro Woche?"
+            />
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Büro-Anteil *</label>
+          <div class="field has-addons">
+            <div class="control">
+              <input
+                type="number"
+                v-model="share"
+                class="input"
+                required="true"
+                placeholder="Wieviel davon verbringen Sie im Büro?"
+              />
+            </div>
+            <p class="control"><span class="button is-static">%</span></p>
+          </div>
+        </div>
+
+        <div class="field is-grouped is-grouped-right">
+          <div class="control">
+            <button type="submit" class="button is-primary">Weiter</button>
+          </div>
+        </div>
+      </form>
+
+      <!-- 2 -->
+
+      <div :class="{ 'is-hidden': step != 2 }" class="content">
+        <div class="mb-3">
+          <p v-if="weeklySavedWorkingHours >= 6" style="background-color: #f00">
+            Du kannst noch einiges an Zeit und Kosten in deinem Betrieb
+            einsparen! Buche dir jetzt einen Call mit mir und lass uns gemeinsam
+            herausfinden, wie du wöchentlich bis zu
+            {{ weeklySavedCosts }} € und {{ weeklySavedWorkingHours }} Stunden
+            einsparen kannst!
+          </p>
+
+          <p
+            v-else-if="weeklySavedWorkingHours >= 3"
+            style="background-color: #ff0"
+          >
+            Da ist noch Potenzial, das du ausschöpfen kannst! Deine wöchentliche
+            Einsparung liegt bei bis zu
+            {{ weeklySavedCosts }} € Lass uns gerne darüber sprechen, wie ich
+            dich noch weiter unterstützen kann.
+          </p>
+
+          <p v-else style="background-color: #0f0">
+            Herzlichen Glückwunsch! Du bist richtig gut aufgestellt: Deine
+            Ersparnis pro Woche liegt bei bis zu {{ weeklySavedCosts }} €
+          </p>
+
+          <p v-if="weeklySavedWorkingHours >= 3">
+            <a :href="consultingLink" class="button is-primary"
+              >Jetzt Beratungsgespräch buchen</a
+            >
+          </p>
+        </div>
+
+        <table class="table is-fullwidth">
+          <caption>
+            Deine aktuelle Zeit- und Kostenbilanz
+          </caption>
+          <thead>
+            <tr>
+              <th>&nbsp;</th>
+              <th>pro Woche</th>
+              <th>pro Monat</th>
+              <th>pro Jahr</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Stunden</th>
+              <td>{{ weeklyWorkingHours }} h</td>
+              <td>{{ perMonth(weeklyWorkingHours) }} h</td>
+              <td>{{ perYear(weeklyWorkingHours) }} h</td>
+            </tr>
+            <tr>
+              <th>Kosten</th>
+              <td>{{ weeklyCosts }} €</td>
+              <td>{{ perMonth(weeklyCosts) }} €</td>
+              <td>{{ perYear(weeklyCosts) }} €</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table is-fullwidth">
+          <caption>
+            Deine mögliche Zukunft nach unserer Zusammenarbeit
+          </caption>
+          <thead>
+            <tr>
+              <th>&nbsp;</th>
+              <th>pro Woche</th>
+              <th>pro Monat</th>
+              <th>pro Jahr</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Zeitersparnis</th>
+              <td>{{ weeklySavedWorkingHours }} h</td>
+              <td>{{ perMonth(weeklySavedWorkingHours) }} h</td>
+              <td>{{ perYear(weeklySavedWorkingHours) }} h</td>
+            </tr>
+            <tr>
+              <th>Kostenersparnis</th>
+              <td>{{ weeklySavedCosts }} €</td>
+              <td>{{ perMonth(weeklySavedCosts) }} €</td>
+              <td>{{ perYear(weeklySavedCosts) }} €</td>
+            </tr>
+          </tbody>
+        </table>
+
         <p>
-          Kontakt: Name = {{ name }} | Company = {{ company }} | Mail =
-          {{ email }} | Phone = {{ phone }}
+          Mit meiner Methode kannst du bis zu
+          {{ perYear(weeklySavedWorkingHours) }} Stunden im Jahr mehr Zeit
+          haben! Zum Beispiel für deine Familie, Freunde, Hobbies, neue Kunden
+          oder strategische Planung, um dein Unternehmen noch erfolgreicher zu
+          machen.
         </p>
+
         <p>
-          Daten: Rate = {{ rate }} | Hours = {{ hours }} | Share = {{ share }}
+          Mit meiner Methode kannst du bis zu
+          {{ perYear(weeklySavedCosts) }} € im Jahr einsparen, die du zum
+          Beispiel in neue Maschinen, Software, in die Weiterbildung deiner
+          Mitarbeiter oder in deine Altersvorsorge, investieren kannst.
+        </p>
+
+        <p v-if="weeklySavedWorkingHours >= 3">
+          <a :href="consultingLink" class="button is-primary"
+            >Jetzt Beratungsgespräch buchen</a
+          >
         </p>
       </div>
     </div>
